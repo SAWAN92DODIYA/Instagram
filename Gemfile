c4dfddd87bfbd9cab225288fc6e112345416fcc3
developer <sawandodiya02@gmail.com>
2024-09-05 19:37:42 +0530

    Add Image using Active Storage

--- Gemfile
+++ Gemfile
@@ -64,3 +64,4 @@ end
 
 gem "devise", "~> 4.9"
 gem 'byebug'
+gem 'world-flags'
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,8 @@
 class HomeController < ApplicationController
   before_action :authenticate_user! 
-  
- 
   def index
+   
+    @posts = Post.where.not(user_id: current_user.id)
     
   end
- 
-
-  
-end
+ end
