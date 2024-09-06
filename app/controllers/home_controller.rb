f365764a58049bcff9a7a9a3b3e2a0d6b1cb3675
developer <sawandodiya02@gmail.com>
2024-09-06 14:35:40 +0530

    Add all_user action in home Controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,10 @@
 class HomeController < ApplicationController
   before_action :authenticate_user! 
   def index
-   
-    @posts = Post.where.not(user_id: current_user.id)
-    
+   @posts = Post.where(user_id: [5,6]).order(created_at: :desc)
   end
+
+  def all_user
+    @users = User.where.not(id: current_user.id )
+  end 
  end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 
   root "home#index"
   resources :posts
+  get "home/users" ,to: "home#all_user"
 
   get "up" => "rails/health#show", as: :rails_health_check
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
