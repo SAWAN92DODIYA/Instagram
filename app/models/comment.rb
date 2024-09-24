e7672ff17ff96a98eacc80b49bf393b857ec52a9
developer <sawandodiya02@gmail.com>
2024-09-24 16:39:31 +0530

    remove notice gem

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,6 @@
 class Comment < ApplicationRecord
    belongs_to :user
    belongs_to :post
-   has_many :notification_mentions, as: :record, dependent: :destroy, class_name: "Noticed::Event"
    validates :body, presence: true
    
 end
--- db/schema.rb
+++ db/schema.rb
@@ -126,30 +126,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_18_062317) do
     t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
-  create_table "noticed_events", force: :cascade do |t|
-    t.string "type"
-    t.string "record_type"
-    t.bigint "record_id"
-    t.jsonb "params"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "notifications_count"
-    t.index ["record_type", "record_id"], name: "index_noticed_events_on_record"
-  end
-
-  create_table "noticed_notifications", force: :cascade do |t|
-    t.string "type"
-    t.bigint "event_id", null: false
-    t.string "recipient_type", null: false
-    t.bigint "recipient_id", null: false
-    t.datetime "read_at", precision: nil
-    t.datetime "seen_at", precision: nil
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["event_id"], name: "index_noticed_notifications_on_event_id"
-    t.index ["recipient_type", "recipient_id"], name: "index_noticed_notifications_on_recipient"
-  end
-
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
